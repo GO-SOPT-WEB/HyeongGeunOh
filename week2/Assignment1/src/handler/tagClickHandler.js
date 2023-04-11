720f2d3679c3fdf4dcd91ad8a7f7f667c1817464
Geun-Oh <kandy1002@naver.com>
2023-04-12 02:59:28 +0900

    refactor: 렌더링 함수 추상화

--- week2/Assignment1/src/handler/tagClickHandler.js
+++ week2/Assignment1/src/handler/tagClickHandler.js
@@ -1,3 +1,5 @@
+import { renderSongCards, renderTagButton, renderTagCheckBox } from "../pages/renderFunction";
+
 export const checkedTags = []
 
 export const tagClickHandler = ({ checked, id }) => {
@@ -7,10 +9,14 @@ export const tagClickHandler = ({ checked, id }) => {
         const idx = checkedTags.indexOf(id)
         if (idx > -1) checkedTags.splice(idx, 1)
     }
+    renderSongCards();
+    renderTagButton();
 };
 
 export const tagButtonClickHandler = ({ innerText }) => {
-    console.log(innerText);
     const idx = checkedTags.indexOf(innerText);
     if (idx > -1) checkedTags.splice(idx, 1);
+    renderTagCheckBox();
+    renderSongCards();
+    renderTagButton();
 };
--- week2/Assignment1/src/pages/main.js
+++ week2/Assignment1/src/pages/main.js
@@ -1,62 +1,37 @@
-import SongCards from "./songCard";
-import TagCheckBox from "./tagCheckBox";
 import {
   tagButtonClickHandler,
   tagClickHandler,
 } from "../handler/tagClickHandler";
-import TagButton from "./tagButton";
-import song from '../constants/song';
+import modalOpenHandler from "../handler/modalOpenHandler";
+import {
+  renderTagCheckBox,
+  renderSongCards,
+  renderTagButton,
+} from "./renderFunction";
+import getData from "../handler/getAddCardData";
 
 function MainPage($container) {
   this.$container = $container;
   this.render = () => {
     const typeWrapper = document.querySelector("#type_wrapper");
-    const cardSection = document.querySelector("#card_section");
     const tagButton = document.querySelector("#tag_button");
+    const cardSection = document.querySelector("#card_section");
+    renderTagCheckBox();
+    renderSongCards();
+    renderTagButton();
 
-    new TagCheckBox(typeWrapper);
-    new SongCards(cardSection);
-    new TagButton(tagButton);
-  
     typeWrapper.addEventListener("click", ({ target }) => {
       tagClickHandler(target);
-      new SongCards(cardSection);
-      new TagButton(tagButton);
     });
     tagButton.addEventListener("click", ({ target }) => {
       tagButtonClickHandler(target);
-      new TagCheckBox(typeWrapper);
-      new SongCards(cardSection);
-      new TagButton(tagButton);
     });
     cardSection.addEventListener("click", ({ target }) => {
-      const name = target.id.split("_")[0];
-      const buttonType = target.id.split("_")[1];
-      if (buttonType === "modalOpenButton") {
-        document.getElementById(`${name}_modal`).style.display = "flex";
-      } else if (buttonType === "modalCloseButton") {
-        document.getElementById(`${name}_modal`).style.display = "none";
-      }
+      modalOpenHandler(target);
     });
-    const getData = () => {
-      const title = localStorage.getItem('title');
-      const tagList = localStorage.getItem('tagList');
-      const imgURL = localStorage.getItem('imgURL');
-      if (title === null) {
-        return;
-      }
-      console.log(title, tagList, imgURL);
-      song.push({
-        name: title,
-        tagList: [tagList],
-        imgURL
-      });
-      new SongCards(cardSection);
-    };
     getData();
   };
-  
   this.render();
-};
+}
 
 export default MainPage;
