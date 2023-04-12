1ef21bbb82b4258c0005607c9bce615b69dc44a6
Geun-Oh <kandy1002@naver.com>
2023-04-13 03:01:09 +0900

    feat: 상수 설정

---
+++ week2/Assignment2/src/assets/todos.js
@@ -0,0 +1,48 @@
+const todos = {
+    "Hyeonggeun": [
+        {
+            content: '베이킹 냠냠',
+            done: false,
+        },
+        {
+            content: '방정리',
+            done: false,
+        },
+    ],
+    "SOPT": [
+        {
+            content: '웹팟 첫 세미나',
+            done: true,
+        },
+        {
+            content: '웹팟 첫 뒤풀이',
+            done: true,
+        },
+        {
+            content: '웹팟 워크샵',
+            done: false,
+        },
+    ],
+    "세종대": [
+        {
+            content: '캡스톤 발표',
+            done: false,
+        },
+        {
+            content: 'UX/UI 디자인',
+            done: false,
+        },
+    ],
+    "운동": [
+        {
+            content: '솝케팝 번개',
+            done: false,
+        },
+        {
+            content: '요가',
+            done: false,
+        },
+    ]
+}
+
+export default todos;
