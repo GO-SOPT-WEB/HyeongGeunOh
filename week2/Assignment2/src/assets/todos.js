72dfa1796373763e1fdb181a0bbf5f6c7489b86b
Geun-Oh <kandy1002@naver.com>
2023-04-13 14:43:52 +0900

    refactor: 상수 포맷 변경

--- week2/Assignment2/src/assets/todos.js
+++ week2/Assignment2/src/assets/todos.js
@@ -1,48 +1,8 @@
 const todos = {
-    "Hyeonggeun": [
-        {
-            content: '베이킹 냠냠',
-            done: false,
-        },
-        {
-            content: '방정리',
-            done: false,
-        },
-    ],
-    "SOPT": [
-        {
-            content: '웹팟 첫 세미나',
-            done: true,
-        },
-        {
-            content: '웹팟 첫 뒤풀이',
-            done: true,
-        },
-        {
-            content: '웹팟 워크샵',
-            done: false,
-        },
-    ],
-    "세종대": [
-        {
-            content: '캡스톤 발표',
-            done: false,
-        },
-        {
-            content: 'UX/UI 디자인',
-            done: false,
-        },
-    ],
-    "운동": [
-        {
-            content: '솝케팝 번개',
-            done: false,
-        },
-        {
-            content: '요가',
-            done: false,
-        },
-    ]
-}
+    "Hyeonggeun": ['베이킹 냠냠', '방정리'],
+    "SOPT": ['웹팟 첫 세미나', '웹팟 첫 뒤풀이', '웹팟 워크샵'],
+    "세종대": ['캡스톤 발표', 'UX/UI 디자인'],
+    "운동": ['솝케팝 번개', '요가'],
+};
 
 export default todos;
