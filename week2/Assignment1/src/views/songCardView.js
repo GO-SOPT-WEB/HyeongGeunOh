a564f253a78627a2ad1f301cf72ab9aa4912e763
Geun-Oh <kandy1002@naver.com>
2023-04-18 15:17:50 +0900

    feat: SongCard 렌더링 시 fade in 애니메이션 적용

--- week2/Assignment1/src/views/songCardView.js
+++ week2/Assignment1/src/views/songCardView.js
@@ -4,7 +4,7 @@ const SongCardView = ({ name, tagList, imgURL }) => {
   const TagBox = tagList.map((tag) => TagBoxView({ tag })).join("");
 
   return `
-    <article>
+    <article style="animation: fadein .5s">
     <h2>${name}</h2>
     <div class="tag_modalButton_wrapper">
     <div class="tag_wrapper">
@@ -12,7 +12,7 @@ const SongCardView = ({ name, tagList, imgURL }) => {
 </div>
 <span class="material-symbols-outlined" id="${name}_modalOpenButton">Add</span>
     </div>
-    <img src=${imgURL} alt="Cover image of blue" />
+    <img src=${imgURL} alt="Cover image of ${name}" />
     <span class="material-symbols-outlined">favorite</span>
     <div class="songCard_modal_wrapper" id="${name}_modal">
       ${TagBox}
