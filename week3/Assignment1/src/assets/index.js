c1c35158d5a7f9dc4b668073c73b4078bae7607a
Geun-Oh <kandy1002@naver.com>
2023-04-26 01:44:53 +0900

    feat: asset 추가

---
+++ week3/Assignment1/src/assets/index.js
@@ -0,0 +1,23 @@
+import IMG_1 from '../assets/IMG_0454';
+import IMG_2 from '../assets/IMG_0455';
+import IMG_3 from '../assets/IMG_0456';
+import IMG_4 from '../assets/IMG_0457';
+import IMG_5 from '../assets/IMG_0459';
+import IMG_6 from '../assets/IMG_0460';
+import IMG_7 from '../assets/IMG_0461';
+import IMG_8 from '../assets/IMG_0463';
+import IMG_9 from '../assets/IMG_0464';
+
+const ImgChunks = {
+  IMG_1,
+  IMG_2,
+  IMG_3,
+  IMG_4,
+  IMG_5,
+  IMG_6,
+  IMG_7,
+  IMG_8,
+  IMG_9,
+};
+
+export default ImgChunks;
