5a38f052b650a333fd9632dc822e780755792d3d
Geun-Oh <kandy1002@naver.com>
2023-04-27 18:21:55 +0900

    chore: vite config 이미지 빌드 수정

--- week3/Assignment1/src/assets/index.js
+++ week3/Assignment1/src/assets/index.js
@@ -1,12 +1,12 @@
-import IMG_1 from '../assets/IMG_0454.png';
-import IMG_2 from '../assets/IMG_0455.png';
-import IMG_3 from '../assets/IMG_0456.png';
-import IMG_4 from '../assets/IMG_0457.png';
-import IMG_5 from '../assets/IMG_0459.png';
-import IMG_6 from '../assets/IMG_0460.png';
-import IMG_7 from '../assets/IMG_0461.png';
-import IMG_8 from '../assets/IMG_0463.png';
-import IMG_9 from '../assets/IMG_0464.png';
+import IMG_1 from './IMG_0454.png';
+import IMG_2 from './IMG_0455.png';
+import IMG_3 from './IMG_0456.png';
+import IMG_4 from './IMG_0457.png';
+import IMG_5 from './IMG_0459.png';
+import IMG_6 from './IMG_0460.png';
+import IMG_7 from './IMG_0461.png';
+import IMG_8 from './IMG_0463.png';
+import IMG_9 from './IMG_0464.png';
 
 const ImgChunks = {
   IMG_1,
