9520a2b19f2dc83944db9588fc9be8eb3c9f4835
Geun-Oh <kandy1002@naver.com>
2023-04-27 20:27:20 +0900

    chore: baseURL 수정

--- week3/Assignment1/vite.config.js
+++ week3/Assignment1/vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), image()],
-  base: '/HyeongGeunOh/',
+  // base: '/HyeongGeunOh/',
 });
