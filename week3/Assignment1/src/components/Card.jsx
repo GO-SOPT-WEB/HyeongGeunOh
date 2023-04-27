7ccdc660afcabd9ce2a1ab6f3076958dabfcfeba
Geun-Oh <kandy1002@naver.com>
2023-04-27 16:16:50 +0900

    feat: 오픈된 카드 클릭 시 이벤트 막기

--- week3/Assignment1/src/components/Card.jsx
+++ week3/Assignment1/src/components/Card.jsx
@@ -6,15 +6,16 @@ const Card = ({ imgURL, index }) => {
   const url = imgURL.split('_')[1];
   const { openCard, correctCard, addOpenCard } = useGlobalContext();
   const format = { url, index };
+  const buttonOnclick = () => {
+    if (openCard.some((x) => x.index === index) || openCard.length === 2) return;
+    addOpenCard(format);
+  };
 
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <article
       className={`card ${correctCard.includes(url) || openCard.some((x) => x.index === index) ? '' : 'rotate'}`}
-      onClick={() => {
-        if (openCard.length === 2) return;
-        addOpenCard(format);
-      }}
+      onClick={buttonOnclick}
       style={{ transformStyle: 'preserve-3d' }}>
       <div className="card_frontface">
         <img src={imgURL} alt={`찌호이미지: ${imgURL}`} />
