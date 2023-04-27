45fda892b620c1b8eaccc224f2b1ed2541f58ed7
Geun-Oh <kandy1002@naver.com>
2023-04-27 16:08:30 +0900

    feat: 카드 두개 이상 열리지 않도록 구현

--- week3/Assignment1/src/components/Card.jsx
+++ week3/Assignment1/src/components/Card.jsx
@@ -11,7 +11,10 @@ const Card = ({ imgURL, index }) => {
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <article
       className={`card ${correctCard.includes(url) || openCard.some((x) => x.index === index) ? '' : 'rotate'}`}
-      onClick={() => addOpenCard(format)}
+      onClick={() => {
+        if (openCard.length === 2) return;
+        addOpenCard(format);
+      }}
       style={{ transformStyle: 'preserve-3d' }}>
       <div className="card_frontface">
         <img src={imgURL} alt={`찌호이미지: ${imgURL}`} />
