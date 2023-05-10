f2c8a2e691b8287192f5041da26899d8438b0a8a
Geun-Oh <kandy1002@naver.com>
2023-05-10 14:31:27 +0000

    refactor: getMonthDate 유틸함수 분리

---
+++ week4/Assginment1/src/utils/getMonthDate.ts
@@ -0,0 +1,6 @@
+export const getMonthDate = () => {
+    const getDate = new Date();
+    const nowMonth = getDate.getMonth() < 10 ? '0' + (getDate.getMonth() + 1) : (getDate.getMonth() + 1)
+    const nowDate = getDate.getDate() < 10 ? '0' + getDate.getDate() : getDate.getDate()
+    return `${nowMonth}-${nowDate}`;
+}
